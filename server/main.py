62a3a3e7b4baa3b12810fe8e5f3927a0a4faec8b
Adithya Bellary <adithya@Adithyas-MacBook-Air.local>
2025-07-01 17:07:28 -0700

    add websocket

--- server/main.py
+++ server/main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, WebSocket
 from pydantic import BaseModel
 from app.client import answer_question
 # import uvicorn
 
-PORT = 8000
+# PORT = 8000
 doc_server = FastAPI()
 
 class QueryRequest(BaseModel):
@@ -13,6 +13,15 @@ class QueryRequest(BaseModel):
 def ask(request: QueryRequest):
     answer = answer_question(request.query)
     return {"answer": answer}
+
+@doc_server.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    print("connected")
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        print("ws data:", data)
+        await websocket.send_text(f"Message was: {data}")
 
 # if __name__ == "__main__":
 #     uvicorn.run("server:doc_server", port=8000, reload=True)
