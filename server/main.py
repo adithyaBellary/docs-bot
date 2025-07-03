8b808bae89a70273503e03681137888af7ee7a56
Adithya Bellary <adithya@Adithyas-MacBook-Air.local>
2025-07-03 15:14:20 -0700

    nits

--- server/main.py
+++ server/main.py
@@ -4,62 +4,44 @@ from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from app.client import answer_question
 from app.ingest import ingest_documentation
-# import uvicorn
 
-# PORT = 8000
 doc_server = FastAPI()
 
 class QueryRequest(BaseModel):
-    query: str
+  query: str
 
 fe_path = "docs-bot-frontend/dist"
 
 @doc_server.post("/ask")
 def ask(request: QueryRequest):
-    answer = answer_question(request.query)
-    return {"answer": answer}
-
-@doc_server.get("/test")
-def test():
-    return {"message": "Hello, World!"}
+  answer = answer_question(request.query)
+  return {"answer": answer}
 
 @doc_server.post("/ingest")
 async def ingest(request: Request):
-    data = await request.json()
-    url = data["url"]
-    ingest_documentation(url)
-    print('ingested:', url)
-    return {"message": f"ingested: {url}"}
+  data = await request.json()
+  url = data["url"]
+  ingest_documentation(url)
+  print('ingested:', url)
+  return {"message": f"ingested: {url}"}
 
 
 messages_archive = []
 @doc_server.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        messages_archive.append({
-            "role": "user",
-            "content": data
-        })
+  await websocket.accept()
+  while True:
+      data = await websocket.receive_text()
+      messages_archive.append({
+        "role": "user",
+        "content": data
+      })
 
-        print(f"Received: {data}")
-
-        # if data == "Hello":
-        #     await websocket.send_text("Hello, what is your name?")
-        # elif data.startswith("My name is "):
-        #     await websocket.send_text(f"Nice to meet you, {data[11:]}!")
-        # else:
-        #     await websocket.send_text(data)
-
-        answer = answer_question(data, messages_archive)
-        messages_archive.append({
-            "role": "assistant",
-            "content": answer
-        })
-        await websocket.send_text(answer)
+      answer = answer_question(data, messages_archive)
+      messages_archive.append({
+        "role": "assistant",
+        "content": answer
+      })
+      await websocket.send_text(answer)
 
 doc_server.mount("/", StaticFiles(directory=fe_path, html=True), name="docs-bot-frontend")
-
-# if __name__ == "__main__":
-#     uvicorn.run("server:doc_server", port=PORT, reload=True)
