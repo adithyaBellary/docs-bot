02ebd7ee74be6b2ee39ae5b3bc640ba50fa36b20
Adithya Bellary <adithya.bellary@gmail.com>
2025-07-03 15:45:13 -0700

    Add chat (#1)
    * add react template and serve statically
    
    * hit the fastAPI server
    
    * connect to ws
    
    * convos
    
    * pass in previous messages context
    
    * nits
    
    * use env var for api domain
    
    ---------
    
    Co-authored-by: Adithya Bellary <adithya@Adithyas-MacBook-Air.local>

---
+++ docs-bot-frontend/src/App.tsx
@@ -0,0 +1,76 @@
+import { useState } from 'react'
+import './App.css'
+
+const domain = process.env.REACT_APP_API_DOMAIN || "localhost"
+
+const SERVER_URL = `http://${domain}:8000`
+const WS_URL = `ws://${domain}:8000/ws`
+
+function App() {
+  const [url, setUrl] = useState<string>()
+  const [fetched, setFetched] = useState<boolean>(false)
+  const [message, setMessage] = useState<string>("")
+  const [messageStore, setMessageStore] = useState<{role: string, content: string}[]>([])
+
+  const scrapeHandler = () => {
+    if (!url) {
+      console.log("NO URL PROVIDED")
+      return
+    }
+    scrape(url).then(d => {
+      console.log(d)
+      setFetched(true)
+    })
+  }
+  const scrape = async (url: string) => {
+    const res = await fetch(`${SERVER_URL}/ingest`, {
+      method: "POST",
+      body: JSON.stringify({ url: url })
+    })
+    return await res.json()
+  }
+
+  const ws = new WebSocket(WS_URL)
+  ws.onopen = () => {
+    console.log('we are in business')
+  }
+
+  ws.onmessage = (event) => {
+    setMessageStore(messages => [...messages, {"role": "assistant", "content": event.data}])
+  }
+
+  const send = () => {
+    messageStore.push({"role": "user", "content": message})
+    setMessage("")
+    ws.send(message)
+  }
+  
+  return (
+    <div>
+      <div>What API documentation would you like to know more about?</div>
+
+      <input onChange={e => setUrl(e.target.value)}/>
+      <button onClick={scrapeHandler}>learn more</button>
+      <div>
+        {fetched && (
+          <>
+            <div>Let us start chatting!</div>
+            
+            {messageStore.map((msg, _) => {
+              return (
+                <div>
+                  <strong>{msg.role}</strong>: {msg.content}
+                </div>
+              )
+              
+            })}
+            <input value={message} onChange={(e) => setMessage(e.target.value)}/> <button onClick={send}>send</button>
+          </>
+        )}
+      </div>
+    </div>
+
+  )
+}
+
+export default App
--- server/main.py
+++ server/main.py
@@ -1,26 +1,47 @@
 from fastapi import FastAPI, Request, WebSocket
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from app.client import answer_question
-# import uvicorn
+from app.ingest import ingest_documentation
 
-# PORT = 8000
 doc_server = FastAPI()
 
 class QueryRequest(BaseModel):
-    query: str
+  query: str
+
+fe_path = "docs-bot-frontend/dist"
 
 @doc_server.post("/ask")
 def ask(request: QueryRequest):
-    answer = answer_question(request.query)
-    return {"answer": answer}
+  answer = answer_question(request.query)
+  return {"answer": answer}
 
+@doc_server.post("/ingest")
+async def ingest(request: Request):
+  data = await request.json()
+  url = data["url"]
+  ingest_documentation(url)
+  print('ingested:', url)
+  return {"message": f"ingested: {url}"}
+
+
+messages_archive = []
 @doc_server.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        answer = answer_question(data)
-        await websocket.send_text(answer)
+  await websocket.accept()
+  while True:
+      data = await websocket.receive_text()
+      messages_archive.append({
+        "role": "user",
+        "content": data
+      })
 
-# if __name__ == "__main__":
-#     uvicorn.run("server:doc_server", port=PORT, reload=True)
+      answer = answer_question(data, messages_archive)
+      messages_archive.append({
+        "role": "assistant",
+        "content": answer
+      })
+      await websocket.send_text(answer)
+
+doc_server.mount("/", StaticFiles(directory=fe_path, html=True), name="docs-bot-frontend")
