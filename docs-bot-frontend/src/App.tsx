33b2e869d3a18aa3478e3dc756d98ce683ef993d
Adithya Bellary <adithya@Adithyas-MacBook-Air.local>
2025-07-02 17:27:35 -0700

    connect to ws

--- docs-bot-frontend/src/App.tsx
+++ docs-bot-frontend/src/App.tsx
@@ -1,29 +1,62 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 const SERVER_URL = "http://localhost:8000"
+const WS_URL = "ws://localhost:8000/ws"
 
 function App() {
-  const [data, setData] = useState(null)
+  const [url, setUrl] = useState<string>()
+  const [fetched, setFetched] = useState<boolean>(false)
+  const [message, setMessage] = useState<string>("")
 
-  const fetchData = async () => {
-    const res = await fetch(`${SERVER_URL}/test`, {
-      method: 'GET'
+  const scrapeHandler = () => {
+    if (!url) {
+      console.log("NO URL PROVIDED")
+      return
+    }
+    scrape(url).then(d => {
+      console.log(d)
+      setFetched(true)
     })
-    const data = await res.json()
-    return data
+  }
+  const scrape = async (url: string) => {
+    const res = await fetch(`${SERVER_URL}/ingest`, {
+      method: "POST",
+      body: JSON.stringify({ url: url })
+    })
+    return await res.json()
   }
 
-  useEffect(() => {
-    try {
-      fetchData().then(d => setData(d.message))
-    } catch (e) {
-      console.log(e)
-    }
-  }, [])
+  const ws = new WebSocket(WS_URL)
+  ws.onopen = () => {
+    console.log('we are in business')
+  }
 
+  ws.onmessage = (event) => {
+    console.log('what we got:', event.data)
+  }
+
+  const send = () => {
+    ws.send(message)
+    console.log('sent', message)
+  }
+  
   return (
-    <div>{data}</div>
+    <div>
+      <div>What API documentation would you like to know more about?</div>
+
+      <input onChange={e => setUrl(e.target.value)}/>
+      <button onClick={scrapeHandler}>learn more</button>
+      <div>
+        {fetched && (
+          <>
+            <div>Let us start chatting!</div>
+            <input onChange={(e) => setMessage(e.target.value)}/> <button onClick={send}>send</button>
+          </>
+        )}
+      </div>
+    </div>
+
   )
 }
 
--- server/main.py
+++ server/main.py
@@ -37,6 +37,7 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
+        print(f"Received: {data}")
         await websocket.send_text(data)
 
         # answer = answer_question(data)
